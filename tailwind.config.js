8518f9ebc6fdcf90ccbe36159c63eb71b8c86f34
Ghazi Muharam <ghazimuharam22@gmail.com>
2021-02-22 09:38:46 +0700

    :tada: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './resources/views/*.edge',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
