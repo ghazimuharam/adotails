45b0b0dea1ad8628f1bcc757a4672758b8861c42
Ghazi Muharam <ghazimuharam22@gmail.com>
2021-05-28 21:15:49 +0700

    :bug: Fixing mongodb support

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -7,9 +7,9 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.string('username', 80)
+      table.string('email', 254)
+      table.string('password', 60)
       table.timestamps()
     })
   }
--- start/app.js
+++ start/app.js
@@ -13,12 +13,12 @@
 const providers = [
   '@adonisjs/framework/providers/AppProvider',
   '@adonisjs/framework/providers/ViewProvider',
-  '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider'
+  '@adonisjs/auth/providers/AuthProvider',
+  'lucid-mongo/providers/LucidMongoProvider'
 ]
 
 /*
@@ -31,7 +31,7 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+  'lucid-mongo/providers/MigrationsProvider',
 ]
 
 /*
