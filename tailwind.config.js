90e85687cb43b4aa5243e3c57d73d92f302a5059
Ghazi Muharam <ghazimuharam22@gmail.com>
2021-06-06 22:37:34 +0700

    :lipstick: Modified landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
   },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hygeia-white': '#F6F7F9',
+        'hygeia-btn-black': '#3D3E40',
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+      },
+      fontFamily: {
+        'display': ['Montserrat', 'sans-serif'],
+      }
+    }
   },
   variants: {
     extend: {},
